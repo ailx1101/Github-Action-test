ece701714cff34901d4c6433300911f1e91e52a6
shzlx <1423925249@qq.com>
2024-02-22 16:12:12 +0800

    fix

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-    with open('iptv.txt','wb')as f:
+    with open('./m3u/iptv.txt','wb')as f:
         f.write(b"123")
         print('文件写入成功')
 
